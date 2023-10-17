2a7e398c8b3fc721b42fd221b3feb87991df1e80
Edgar <edgar.nurmagomedov@mail.ru>
2023-10-17 19:51:54 +0500

    Lesson 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,51 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    //Задание 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    //Задание 2
+    public static void checkSumSign() {
+        int a = 10;
+        int b = 15;
+
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    //Задание 3
+    public static void printColor() {
+        int value = 100;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    //Задание 4
+    public static void compareNumbers() {
+        int a = -44;
+        int b = 65;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 }
