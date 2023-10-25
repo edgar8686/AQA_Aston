5fbd0d5a6fbbc744b567e86621eba5b207cb270a
Edgar <edgar.nurmagomedov@mail.ru>
2023-10-25 18:20:29 +0500

    Lesson 3

--- src/main/java/org/example/lesson2/PrintWords.java
+++
@@ -1,9 +0,0 @@
-package org.example.lesson2;
-
-public class PrintWords {
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-}
