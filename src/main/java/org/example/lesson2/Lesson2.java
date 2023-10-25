bc63d0413be0151d173e2e44efb887ea6d4c55fb
Edgar <edgar.nurmagomedov@mail.ru>
2023-10-25 18:08:00 +0500

    Lesson 2

--- src/main/java/org/example/lesson2/Lesson2.java
+++ src/main/java/org/example/lesson2/Lesson2.java
@@ -2,51 +2,13 @@ package org.example.lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-    }
-
-    //Задание 1
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
-    //Задание 2
-    public static void checkSumSign() {
-        int a = 10;
-        int b = 15;
-
-        if (a + b >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
-    }
-
-    //Задание 3
-    public static void printColor() {
-        int value = 100;
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
-        } else if (value > 100) {
-            System.out.println("Зеленый");
-        }
-    }
-
-    //Задание 4
-    public static void compareNumbers() {
-        int a = -44;
-        int b = 65;
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
-        }
+        //Задание 1
+        PrintWords.printThreeWords();
+        //Задание 2
+        SumSign.checkSumSign();
+        //Задание 3
+        Color.printColor();
+        //Задание 4
+        Numbers.compareNumbers();
     }
 }
---
+++ src/main/java/org/example/lesson2/PrintWords.java
@@ -0,0 +1,9 @@
+package org.example.lesson2;
+
+public class PrintWords {
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
